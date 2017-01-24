d85d180b63af2b45d3d683f352208affe7113cdd
leonspok <igor9774@gmail.com>
2017-01-24 10:27:44 +0300

    Initial Commit

---
+++ OpenGLClone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLClone
+//
+//  Created by Игорь Савельев on 24/01/2017.
+//  Copyright © 2017 MusicSense. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OpenGLClone/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OpenGLClone
+//
+//  Created by Игорь Савельев on 24/01/2017.
+//  Copyright © 2017 MusicSense. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
